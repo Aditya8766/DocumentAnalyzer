c72ca726fe95a41a8ad847898997a8cac71c5871
Aditya8766 <111683825+Aditya8766@users.noreply.github.com>
2024-03-14 07:52:43 +0530

    updated-code

--- Document-Analyzer-Server/constants/mailBody.js
+++ Document-Analyzer-Server/constants/mailBody.js
@@ -1,28 +1,26 @@
 const mailBody = {
-  getUserMailBody: (payload) => {
-    const { name } = payload;
-
-    return `<div>
-                <h1>Dear ${name},</h1><br/>
-                <h2>Welcome Praise Array - Unleash your Web Development Superpowers!</h2>
-                <p>Welcome to DocPro! We are thrilled to have you on board as a registered user of our web development platform. Get ready to dive into a world of coding, creativity, and endless possibilities!</p>
-                <p>Happy coding!</p>
-                <p>Best regards,</p>
-                <h3>DocPro Team</h3>
-            </div>`;
-  },
-  getAdminMailBody: (payload)=>{
-    const {userName, email, phoneNumber, businessName, businessDescription, workType, currentWebsiteUrl} = payload;
-    return `<div>
-                <p>Name: ${userName},</p><br/>
-                <p>Email: ${email},</p><br/>
-                <p>Phone Number: ${phoneNumber},</p><br/>
-                <p>Buisness Name: ${businessName},</p><br/>
-                <p>Buisness Description: ${businessDescription},</p><br/>
-                <p>Work Type: ${workType},</p><br/>
-                <p>Current Website URL:  ${currentWebsiteUrl},</p><br/>
-            </div>`;
-  },
-};
-
-module.exports = mailBody;
+    getResetPasswordBody: (payload) => {
+      const { name, link } = payload;
+  
+      return `<div>
+                  <h1>Hey ${name},</h1><br/>
+                  <h3>This is link for reset your password and it will be valid for 30 min</h3>
+                  <a href="${link}">Click Here</a>
+                  <h3><b>DocPro - Project Document Analyzer</b></h3>
+              </div>`;
+    },
+    getRegisterBody: (payload) => {
+      const { name } = payload;
+  
+      return `<div>
+                  <h1>Dear ${name},</h1><br/>
+                  <h2>Welcome to DocPro | Project Document Analyzer - Unleash your Web Development Superpowers!</h2>
+                  <p>Welcome to DocPro! We are thrilled to have you on board as a registered user of our web development platform. Get ready to dive into a world of coding, creativity, and endless possibilities!</p>
+                  <p>Happy coding!</p>
+                  <p>Best regards,</p>
+                  <h3>DocPro Team</h3>
+              </div>`;
+    },
+  };
+  
+  module.exports = mailBody;
--- Document-Analyzer-Server/index.js
+++ Document-Analyzer-Server/index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
-const nodemailer = require("nodemailer");
-const mailBody = require("./constants/mailBody");
+require("./db/conn");
+
+const router = require("./routes/router");
 
 const app = express();
 const port = 5050;
@@ -11,69 +12,10 @@ const port = 5050;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors());
-
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: { 
-    user: process.env.PRAISE_ARRAY_EMAIL,
-    pass: process.env.PRAISE_ARRAY_PASSWORD,
-  },
-});
+app.use(router);
 
 app.get("/", (req, res) => {
-  res.send("PraiseArray server working");
-});
-
-app.post("/register", async (req, res) => {
-  console.log("Register:::", req.body);
-
-  const { userName, email, phoneNumber, businessName, businessDescription, workType, currentWebsiteUrl } = req.body;
-
-  if (!userName || !email || !phoneNumber || !businessName) {
-    console.log("Fields are missing in the request:", req.body);
-    return res.status(400).json({ error: "All fields are required !" });
-  }
-
-  try {
-    const UserMailOptions = {
-      from: process.env.PRAISE_ARRAY_EMAIL,
-      to: email,
-      subject: "Welcome to PraiseArray ",
-      html: mailBody.getUserMailBody({ name: userName }),
-    };
-
-    const AdminMailOptions = {
-      from: process.env.PRAISE_ARRAY_EMAIL,
-      to: 'abhiseknlandge@gmail.com',
-      subject: `${userName} wants to connect with PraiseArray`,
-      html: mailBody.getAdminMailBody({ userName, email, phoneNumber, businessName, businessDescription, workType, currentWebsiteUrl }),
-    };
-
-    transporter.sendMail(UserMailOptions, (userMailError, userData) => {
-      if (userMailError) {
-        console.log("Error sending user mail =>", userMailError);
-      } else {
-        console.log("User Mail sent successfully", userData);
-      }
-    });
-
-    transporter.sendMail(AdminMailOptions, (adminMailError, adminData) => {
-      if (adminMailError) {
-        console.log("Error sending admin mail =>", adminMailError);
-      } else {
-        console.log("Admin Mail sent successfully", adminData);
-      }
-    });
-
-    res.status(201).json({
-      status: 201,
-      message: "User Registered Successfully",
-    });
-
-  } catch (error) {
-    console.log("Error:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+  res.send("Document server working");
 });
 
 app.listen(port, () => {
